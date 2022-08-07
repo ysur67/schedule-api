5a5e0b2bdb5bd9a89277f6da04fd09d9811ad5d8
ysur67 <rus.muratow@gmail.com>
2022-08-07 20:32:20 +0300

    remove redundant code

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
-from datetime import datetime
-
 from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 
 from core.dependencies import get_db
-from modules.lessons_parser.http.base import RequestType
-from modules.lessons_parser.http.groups_parser import GroupsParser
-from modules.lessons_parser.http.lessons_parser import LessonsParser
-from utils.date_time import to_message_format
 
 app = FastAPI()
 
 
 @app.get("/")
 async def index(db: Session = Depends(get_db)):
-    lessons_start_date = datetime.strptime("01.01.2022", "%d.%m.%Y")
-    lessons_end_date = datetime.strptime("01.03.2022", "%d.%m.%Y")
-    payload = {
-        "rtype": "3",
-        "ucstep": "1",
-        "exam": "0",
-        "datafrom": to_message_format(lessons_start_date),
-        "dataend": to_message_format(lessons_end_date),
-        "formo": "2",
-        "formob": "0",
-        "prdis": "0"
-    }
-    parser = await LessonsParser.build_parser(
-        "http://inet.ibi.spb.ru/raspisan/rasp.php",
-        payload_data=payload,
-        db=db,
-    )
-    await parser.parse()
     return {"message": "hello"}
